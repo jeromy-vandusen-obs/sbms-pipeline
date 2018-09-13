885e5e82dd64b0e3b45350ef0a7a9d240824f198
Jeromy Van Dusen <JVanDusen@obsglobal.com>
2018-09-13 08:58:07 -0500

    Add subnet to network creation script.

--- create-network.sh
+++ create-network.sh
@@ -1,1 +1,2 @@
-docker -H $NETWORK_HOST network create -d overlay -o encrypted --attachable $NETWORK_NAME || true
+#!/usr/bin/env bash
+docker -H $NETWORK_HOST network create -d overlay -o encrypted --attachable --subnet 10.0.0.0/24 $NETWORK_NAME || true
--- remove-network.sh
+++ remove-network.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 docker -H $NETWORK_HOST network rm $NETWORK_NAME || true
