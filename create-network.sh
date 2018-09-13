aa9d13a995922f074f71344bf9881355891a35df
Jeromy Van Dusen <JVanDusen@obsglobal.com>
2018-09-13 09:20:29 -0500

    Move infrastructure back into main deployment.

--- create-network.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker -H $NETWORK_HOST network create -d overlay -o encrypted --attachable --subnet 10.0.0.0/24 $NETWORK_NAME || true
--- remove-network.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker -H $NETWORK_HOST network rm $NETWORK_NAME || true
